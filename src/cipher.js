ed8c698bf021551936ef7677886c3650657cd98e
AndreVivs <75138276+AndreVivs@users.noreply.github.com>
2021-01-08 13:34:28 -0600

    Este es el codgo del Proyecto Cifer sin Testeo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,56 @@
 const cipher = {
-  // ...
-};
+  
+  //Encode Text
+  encode: function (txtToCode,numero){
+    alert ("encode text")
 
+    let newString="";
+  for (let c=0; c < txtToCode.length; c++){
+    let asciiTxt = txtToCode.charCodeAt(c);
+  if (asciiTxt >= 65 && asciiTxt <= 90){
+    let newAsciiTxt = (asciiTxt - 65 + numero)% 26 + 65;
+    newString += String.fromCharCode(newAsciiTxt);
+  } 
+  }
+  return newString;
+
+},
+
+//Decode partner text & Mensajes de despedida
+decode : function (partnerTxt,partnerNmb){
+  let newString="";
+for (let c=0; c < partnerTxt.length; c++){
+  let asciiPtxt = partnerTxt.charCodeAt(c);
+if (asciiPtxt >= 65 && asciiPtxt <= 90){
+  let newAsciiPtxt = (asciiPtxt - 65 - partnerNmb)% 26 + 65;
+newString += String.fromCharCode(newAsciiPtxt);
+} 
+}
+return newString
+},
+  //Encode Name
+  encodename : function (name, off) {
+
+    alert("encode name");
+
+      let newString = "";
+    for (let i = 0; i < name.length; i++) {
+      let asciiName = name.charCodeAt(i);
+      let newAsciiName = (asciiName - 65 + off) % 26 + 65;
+      newString += String.fromCharCode(newAsciiName);
+    }
+    return newString;
+  },
+  
+  //Decode partner name
+  decodepartnername : function (partnerName,off){
+      let newStringp="";
+    for (let c=0; c < partnerName.length; c++){
+      let asciiPname = partnerName.charCodeAt(c);
+      let newAsciiPname = (asciiPname - 65 - off)% 26 + 65;
+      newStringp += String.fromCharCode(newAsciiPname);
+    }
+    return newStringp
+  }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,86 +1,38 @@
-//import cipher from './cipher.js';
+import cipher from './cipher.js';
+//Encode name
+document.getElementById("send").addEventListener("click", function(){
+  alert('cifrando nombre')
+  let off=3;
+  let name=document.getElementById("name").value;
+  let encode=cipher.encodename(name,off);
+document.getElementById('cont3').innerHTML=' YOUR LOGING NAME IS '+ encode ;
+})
+//Encode text
+document.getElementById("cif").addEventListener("click", function(){
+  alert ("cifrando texto")
+  let numero= parseInt(document.getElementById("num").value);
+  let txtToCode=document.getElementById("txtToCode").value;
+  let encode=cipher.encode(txtToCode,numero);
+document.getElementById('txtCode').value=encode;
+})
+//Decode partner name
+document.getElementById("decode").addEventListener("click",function(){
+  alert('descifrando nombre')
+  let off=3;
+  let partnerName=document.getElementById("logingname").value;
+  let decode=cipher.decodepartnername(partnerName,off);
+document.getElementById('partnername').value=decode;
+})
+//Decode partner text & Mensajes de despedida
+document.getElementById("decode").addEventListener("click",function(){
+  alert('descifrando texto')
+  let partnerNmb= parseInt(document.getElementById("partnum").value);
+  let partnerTxt = document.getElementById("partnerTxt").value;
+  let decode=cipher.decode(partnerTxt,partnerNmb);
+document.getElementById('partnerTxtDec').value=decode;
+//Estos son los mensajes de despedida.
+let name=document.getElementById("name").value;
+document.getElementById('cont8').innerHTML='¡YAAAAY, LO HAS LOGRADO ' + name + '!'
+document.getElementById('cont9').innerHTML='DISFRUTA TUS VACACIONES, NOS VEMOS EL PROXIMO AÑO CON TODO EL ÁNIMO DEL MUNDO'
 
-//console.log(cipher);
-function encodeName (){
-    let off=3;
-    let name=document.getElementById("txt").value;
-    console.log(name);
-    document.getElementById('cont1').innerHTML='¡Apartir de ahora dejas de ser '+ name +' para se conocido como...!';
-  
-    alert ("hola")
-  let newString="";
-  for (let c=0; c < name.length; c++){
-    let asciiName = name.charCodeAt(c);
-  console.log(asciiName);
-  let newAsciiName = (asciiName-65+off)% 26 + 65;
-  console.log(newAsciiName);
-  newString += String.fromCharCode(newAsciiName);
-  console.log(newString);
-  document.getElementById('cont2').innerHTML=newString;
-  document.getElementById('cont3').innerHTML='BIENVENIDO '+ newString + ' VAMOS A CIFRAR ESE MENSAJE';
-  }
-  return newString;
-  }
-  
-  function encodeTxt(){
-    let numero= parseInt(document.getElementById("num").value);
-    console.log(numero);
-    let txtToCode=document.getElementById("txtCode").value;
-    console.log(txtToCode);
-  
-  alert ("holis")
-  
-  let newString="";
-  for (let c=0; c < txtToCode.length; c++){
-    let asciiTxt = txtToCode.charCodeAt(c);
-  console.log(asciiTxt);
-  let newAsciiTxt = (asciiTxt-65+numero)% 26 + 65;
-  console.log(newAsciiTxt);
-  newString += String.fromCharCode(newAsciiTxt);
-  console.log(newString);
-  document.getElementById('cont4').innerHTML=newString;
-  }
-  return newString;
-  }
-  
-  function decode (){
-    let name=document.getElementById("txt").value;
-    console.log(name);
-  document.getElementById('cont5').innerHTML='¡Perfecto! ' + name + ' parece que todo esta regresando a la normalidad.';
-  
-   let partnerNmb= parseInt(document.getElementById("partnum").value);
-    console.log(partnum);
-    let partnerTxt = document.getElementById("text").value;
-    console.log(partnerTxt);
-  
-    alert ("hola")
-  
-  let newString="";
-  for (let c=0; c < partnerTxt.length; c++){
-    let asciiPtxt = partnerTxt.charCodeAt(c);
-  console.log(asciiPtxt);
-  let newAsciiPtxt = (asciiPtxt-65-partnerNmb)% 26 + 65;
-  console.log(newAsciiPtxt);
-  newString += String.fromCharCode(newAsciiPtxt);
-  console.log(newString);
-  document.getElementById('cont6').innerHTML=newString;
-  }
-  
-    let off=3;
-    let partnerName=document.getElementById("partner").value;
-    console.log(partnerName);
-  
-    alert ("hola")
-  
-  let newStringp="";
-  for (let c=0; c < partnerName.length; c++){
-    let asciiPname = partnerName.charCodeAt(c);
-  console.log(asciiPname);
-  let newAsciiPname = (asciiPname-65-off)% 26 + 65;
-  console.log(newAsciiPname);
-  newStringp += String.fromCharCode(newAsciiPname);
-  console.log(newStringp);
-  document.getElementById('cont7').innerHTML=newStringp;
-  }
-  document.getElementById('cont8').innerHTML='¡YAAAAY! LO HAS LOGRADO. DISFRUTA TUS VACACIONES, NOS VEMOS EL PROXIMO AÑO CON TODO EL ÁNIMO DEL MUNDO'
-  }
+})
